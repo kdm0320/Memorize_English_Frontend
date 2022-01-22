38a1f15cc437db2742e1b31237c20cb2d4bc1974
Dongmin <kdm3578@gmail.com>
2022-01-22 16:25:48 +0900

    update_8 게시판작성

--- src/Routes/Write.tsx
+++ src/Routes/Write.tsx
@@ -1,15 +1,69 @@
 import { Link, useLocation } from "react-router-dom";
+import styled from "styled-components";
+
+const Title = styled.div`
+  width: 90%;
+  height: 30px;
+  border-color: black;
+  border-width: 1px;
+  border-style: dashed;
+  text-align: center;
+  margin-bottom: 10px;
+`;
+
+const Content = styled.div`
+  display: flex;
+  width: 90%;
+  height: 300px;
+  border-color: black;
+  border-width: 1px;
+  border-style: outset;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Img = styled.img`
+  width: 100px;
+  height: 100px;
+  border-color: black;
+  border-style: solid;
+  border-width: 1px;
+  margin-top: 10px;
+`;
+
+const Choice = styled.span``;
 
 function Write() {
   const search = useLocation();
 
   return (
     <div>
-      {search.pathname === "/qna/write" ? (
-        <h1>버그리포트</h1>
-      ) : (
-        <h1>유저리포트</h1>
-      )}
+      <h1>{search.pathname === "/qna/write" ? "버그리포트" : "유저리포트"}</h1>
+      <Title>제목</Title>
+      <Content>
+        <input
+          placeholder="내용을 입력하세요"
+          style={{ border: "0", outline: "0" }}
+        />
+      </Content>
+      <span>
+        <Img></Img>
+      </span>
+      <span>
+        <Choice>
+          <label>
+            공개
+            <input type="checkbox" name="공개" id="close" value="공개" />
+          </label>
+        </Choice>
+        <Choice>
+          <label>
+            비공개
+            <input type="checkbox" name="비공개" id="close" value="비공개" />
+          </label>
+        </Choice>
+      </span>
+      <button>작성</button>
     </div>
   );
 }
