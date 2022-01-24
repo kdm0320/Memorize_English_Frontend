c0a56b5c17c540f63bb326ec23b56352ee478ddc
Dongmin <kdm3578@gmail.com>
2022-01-24 17:58:37 +0900

    signup validation_(Pt1)

--- src/Routes/SignUp.tsx
+++ src/Routes/SignUp.tsx
@@ -4,6 +4,7 @@ import { useForm } from "react-hook-form";
 import { useMutation, useQuery } from "react-query";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { collapseTextChangeRangesAcrossMultipleVersions } from "typescript";
 import { baseUrl } from "../api";
 
 interface IForm {
@@ -20,54 +21,80 @@ const InPut = styled.input.attrs((props) => ({
 }))``;
 
 function SignUp() {
-  const { register, handleSubmit } = useForm<IForm>();
-  const [loading, setLoading] = useState(true);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IForm>();
   const mutation = useMutation((newUser: IForm) =>
     axios.post(`${baseUrl}/users/`, newUser)
   );
   const onValid = async (data: IForm) => {
-    try {
-      mutation.mutateAsync({
-        username: data.username,
-        password: data.password,
-        first_name: data.first_name,
-        last_name: data.last_name,
-        email: data.email,
-        nickname: data.nickname,
-      });
-      console.log(data);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      console.log("done");
-    }
+    // try {
+    //   mutation.mutateAsync({
+    //     username: data.username,
+    //     password: data.password,
+    //     first_name: data.first_name,
+    //     last_name: data.last_name,
+    //     email: data.email,
+    //     nickname: data.nickname,
+    //   });
+    // } catch (error) {
+    //   console.error(error);
+    // } finally {
+    //   console.log("done");
+    // }
   };
+
   return (
     <div>
       <form onSubmit={handleSubmit(onValid)}>
         <InPut
-          {...register("first_name", { required: true })}
+          {...register("first_name", { required: "필수 항목입니다." })}
           placeholder="성(First Name)"
         />
+        <span>{errors.first_name?.message}</span>
         <InPut
-          {...register("last_name", { required: true })}
+          {...register("last_name", { required: "필수 항목입니다." })}
           placeholder="이름(Last Name)"
         />
+        <span>{errors.last_name?.message}</span>
         <InPut
-          {...register("username", { required: true })}
+          {...register("username", {
+            required: "필수 항목입니다.",
+            pattern: {
+              value: /^[a-z]+[a-z0-9]{5,19}$/g,
+              message: "영문자로 시작하는 6~20자 영문자 또는 숫자여야합니다. ",
+            },
+          })}
           placeholder="아이디(ID)"
           type="text"
         />
+        <span>{errors.username?.message}</span>
         <InPut
-          {...register("email", { required: true })}
+          {...register("email", {
+            required: "필수 항목입니다.",
+            pattern: {
+              value: /@/,
+              message: "유효한 형식을 입력하십시오",
+            },
+          })}
           placeholder="이메일(email)"
           type="text"
         />
+        <span>{errors.email?.message}</span>
         <InPut
-          {...register("password", { required: true })}
+          {...register("password", {
+            required: "필수 항목입니다.",
+            pattern: {
+              value: /(?=.*\d)(?=.*[a-z]).{8,}/,
+              message: "영어 소문자, 숫자 포함 8자 이상으로 입력해 주십시오",
+            },
+          })}
           placeholder="비밀번호(Password)"
           type="password"
         />
+        <span>{errors.password?.message}</span>
         <InPut {...register("nickname")} placeholder="닉네임(Nickname)" />
         <button>Create</button>
       </form>
